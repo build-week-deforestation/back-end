926e1cdc900b49722f72101de42a83bed07a9110
Miguel Peralta <eudysperalta723@gmail.com>
2019-10-24 20:40:22 -0400

    clean up code and make routes work

---
+++ auth/auth-router.js
@@ -0,0 +1,80 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const db = require('../database/dbConfig.js');
+const restricted = require('./authenticate-middleware.js')
+const secrets = require('../secrets.js')
+
+
+
+router.get('/users', (req, res) => {
+  db('users')
+  .then(users => {
+    res.send(users)
+  })
+});
+
+router.get('/test-route', restricted, (req, res) => {
+  res.send("there is a token!!")
+});
+
+
+
+router.post('/register', (req, res) => {
+  const newMember = req.body;
+  newMember.password = bcrypt.hashSync(newMember.password, 12)
+
+  db('users').insert(newMember)
+  .then(registeredUser => {
+        const token = generateToken(registeredUser)
+        res.status(200).json({
+            user: registeredUser,
+            token
+        })
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: `${error}`
+        })
+    })
+
+});
+
+router.post('/login', (req, res) => {
+  const {username , password} = req.body;
+    db('users').where({username})
+    .first()
+    .then(user => {
+        if(user && bcrypt.compareSync(password , user.password)){
+            const token = generateToken(user)
+            res.status(200).json({
+                message: `welcome ${user.username}`,
+                token
+            })
+        } else {
+            res.status(404).json({
+                message: `user not found`
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: `${err}`
+        })
+    })
+});
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: '1d',
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
+module.exports = router;
