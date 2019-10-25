361879f30ff3a62257e871f20fcca34e597f3d25
Miguel Peralta <eudysperalta723@gmail.com>
2019-10-24 22:21:50 -0400

    init gitignore and production knexfile

--- knexfile.js
+++ knexfile.js
@@ -9,4 +9,14 @@ module.exports = {
     },
     seeds: { directory: './database/seeds' },
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './database/seeds' },
+  },
 };
--- secrets.js
+++ secrets.js
@@ -1,3 +1,4 @@
 module.exports = {
   jwtSecret: process.env.JWT_SECRET || 'add a third table for many to many',
+  environment: process.env.NODE_ENV || 'development'
 };
